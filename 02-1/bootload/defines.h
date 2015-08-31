a66b54706aafff5337db8b19aff21a23a5a811b5
satoshishimizu <sto_5359@yahoo.co.jp>
2015-08-31 22:28:06 +0900

    LED 1sec Lighting

--- 02-1/bootload/defines.h
+++ 02-1/bootload/defines.h
@@ -7,5 +7,141 @@
 typedef unsigned char  uint8;
 typedef unsigned short uint16;
 typedef unsigned long  uint32;
+/**************************************************/
+/**       Common                                 **/
+/**************************************************/
+#define DISABLE 0x00
+#define ENABLE	0x01
+
+/**************************************************/
+/**       LED                                    **/
+/**************************************************/
+
+#define LED_ON	0x01
+#define LED_OFF	0x00
+/**************************************************/
+/**       1Byte Struct                           **/
+/**************************************************/
+
+typedef union{
+	
+	unsigned char BYTE;
+	
+	struct {
+		
+		unsigned char B7:1;
+		unsigned char B6:1;
+		unsigned char B5:1;
+		unsigned char B4:1;
+		unsigned char B3:1;
+		unsigned char B2:1;
+		unsigned char B1:1;
+		unsigned char B0:1;
+		
+	} BIT;
+	
+} _IOREG;
+
+/**************************************************/
+/**       I/O port regster                       **/
+/**************************************************/
+#define ABWCR (*(volatile _IOREG *)0xfee020)
+#define P4DDR (*(volatile _IOREG *)0xfee003)
+#define P4DR  (*(volatile _IOREG *)0xffffd3)
+#define P4PCR (*(volatile _IOREG *)0xfee03e)
+
+/**************************************************/
+/**                  Timer                       **/
+/**************************************************/
+
+typedef union{
+	
+	unsigned char BYTE;
+	
+	struct {
+		
+		unsigned char RESERVE0	:1;
+		unsigned char IMIE		:3; // IMFフラグによる割り込み 許可/禁止 設定
+		unsigned char RESERVE1	:1;
+		unsigned char IMF		:3; // GRの発生を示すステータスフラグ
+		
+	} BIT;
+	
+} _TISR;
+
+typedef union{
+	
+	unsigned char BYTE;
+	
+	struct {
+		
+		unsigned char RESERVE	:1;
+		unsigned char CCLR		:2;
+		unsigned char CKEG		:2;
+		unsigned char TPSC		:3;
+		
+	} BIT;
+	
+} _16TCR;
+
+
+typedef union{
+	
+	unsigned char BYTE;
+	
+	struct {
+		
+		unsigned char RESERVE0	:1;
+		unsigned char IOB		:3;
+		unsigned char REVER1	:1;
+		unsigned char IOA		:3;
+		
+	} BIT;
+	
+} _16TIOR;
+
+
+/**                  Common                      **/
+
+#define T_TSTR 		(*(volatile _IOREG	*)0xffff60)
+#define T_TSNC 		(*(volatile _IOREG	*)0xffff61)
+#define T_TMDR 		(*(volatile _IOREG	*)0xffff62)
+#define T_TOLR 		(*(volatile _IOREG	*)0xffff63)
+#define T_TISRA		(*(volatile _TISR	*)0xffff64)
+#define T_TISRB		(*(volatile _TISR	*)0xffff65)
+#define T_TISRC		(*(volatile _TISR	*)0xffff66)
+
+/**                  Ch0                       **/
+
+#define T_16TCR0 	(*(volatile _16TCR 	*)0xffff68)
+#define T_TIOR0 	(*(volatile _16TIOR *)0xffff69)
+#define T_16TCNT0_H (*(volatile _IOREG 	*)0xffff6A)
+#define T_16TCNT0_L (*(volatile _IOREG 	*)0xffff6B)
+#define T_GRA0_H 	(*(volatile _IOREG 	*)0xffff6C)
+#define T_GRA0_L 	(*(volatile _IOREG 	*)0xffff6D)
+#define T_GRB0_H 	(*(volatile _IOREG 	*)0xffff6E)
+#define T_GRB0_L 	(*(volatile _IOREG 	*)0xffff6F)
+
+/**                  Ch1                       **/
+
+#define T_16TCR1 	(*(volatile _16TCR 	*)0xffff70)
+#define T_TIOR1 	(*(volatile _16TIOR *)0xffff71)
+#define T_16TCNT1_H (*(volatile _IOREG 	*)0xffff72)
+#define T_16TCNT1_L (*(volatile _IOREG 	*)0xffff73)
+#define T_GRA1_H 	(*(volatile _IOREG 	*)0xffff74)
+#define T_GRA1_L 	(*(volatile _IOREG 	*)0xffff75)
+#define T_GRB1_H 	(*(volatile _IOREG 	*)0xffff76)
+#define T_GRB1_L 	(*(volatile _IOREG 	*)0xffff77)
+
+/**                  Ch2                       **/
+
+#define T_16TCR2 	(*(volatile _16TCR 	*)0xffff78)
+#define T_TIOR2 	(*(volatile _16TIOR *)0xffff79)
+#define T_16TCNT2_H (*(volatile _IOREG 	*)0xffff7A)
+#define T_16TCNT2_L (*(volatile _IOREG 	*)0xffff7B)
+#define T_GRA2_H 	(*(volatile _IOREG 	*)0xffff7C)
+#define T_GRA2_L 	(*(volatile _IOREG 	*)0xffff7D)
+#define T_GRB2_H 	(*(volatile _IOREG 	*)0xffff7E)
+#define T_GRB2_L 	(*(volatile _IOREG 	*)0xffff7F)
 
 #endif
--- 02-1/bootload/main.c
+++ 02-1/bootload/main.c
@@ -2,72 +2,110 @@
 #include "serial.h"
 #include "lib.h"
 
-typedef union{
-	
-	unsigned char BYTE;
-	
-	struct {
-		
-		unsigned char B7:1;
-		unsigned char B6:1;
-		unsigned char B5:1;
-		unsigned char B4:1;
-		unsigned char B3:1;
-		unsigned char B2:1;
-		unsigned char B1:1;
-		unsigned char B0:1;
-		
-	} BIT;
-	
-} _IOREG;
-
-
-/**************************************************/
-/**       I/O port regster                       **/
-/**************************************************/
-#define ABWCR (*(volatile _IOREG *)0xfee020)
-#define P4DDR (*(volatile _IOREG *)0xfee003)
-#define P4DR  (*(volatile _IOREG *)0xffffd3)
-#define P4PCR (*(volatile _IOREG *)0xfee03e)
 /**************************************************/
 
-/**************************************************/
-/**       wait function                          **/
-/**************************************************/
 
-void wait(unsigned int ms)
+unsigned int orOpm(unsigned int a, unsigned int vaule)
 {
+	unsigned int reuslt = 0;
+	return reuslt = a | vaule;
 	
-	unsigned int i = ms;
-	
-	for ( i = 0; i > 10000; i++)
-	{
-		
-	}
-
 }
 
 int main(void)
 {
 	serial_init(SERIAL_DEFAULT_DEVICE);
 
-	puts("-----------------Led1 and 2 is Light start!!\n");
-	
-	unsigned char *p4ddr = (unsigned char *)0xfee003;
-	unsigned char *p4dr  = (unsigned char *)0xffffd3;
-	
-	puts("*p4ddr=");	putxval(*p4ddr, 0); 	puts("\n");
-	puts("*p4dr=");		putxval(*p4dr, 0); 		puts("\n");
-	puts("----------------------------------------------\n");
+	puts("-----------------Led1 and 2 is Light start!!\n");	
 	/* p4ddr(P46 and P47) output setting 0x0c = 1100 0000  */
 	/* p4dr (P46 and P47) output         0x0c = 1100 0000  */
-	*p4ddr = 0xc0;
-	*p4dr  = 0xc0;
-	puts("----------------------------------------------\n");	
-	puts("*p4ddr=");	putxval(*p4ddr, 0); 	puts("\n");
-	puts("*p4dr=");		putxval(*p4dr, 0); 		puts("\n");
 
-	puts("-----------------end\n");
+	P4DDR.BYTE		= 0;
+	P4DDR.BIT.B6	= ENABLE;
+	P4DDR.BIT.B7	= ENABLE;
 
-  return 0;
+	P4DR.BYTE		= DISABLE;
+	
+	P4DR.BIT.B6 	= LED_ON;
+	P4DR.BIT.B7 	= LED_OFF;
+	
+	/**
+	 * カウンター
+	*/
+	int T_16TCR0_COUNTER = 0;
+	int secCount = 0;
+	
+	/**
+	 * カウントクロック設定 φ/8
+	 */
+	T_16TCR0.BIT.TPSC = 0x03;
+	//T_16TCR1.BIT.TPSC = 0x03;
+	
+	/**
+	 * カウンタクリア要因の選択
+	 */
+	T_16TCR0.BIT.CCLR = 0x01; // GRA
+	//T_16TCR1.BIT.CCLR = 0x01; // GRA
+	
+	/**
+	 * アウトプットコンペアレジスタの選択
+	 */
+	T_TIOR0.BIT.IOA = 0x02; // GRA コンペアマッチ 1出力
+	//T_TIOR1.BIT.IOA = 0x02; // GRA コンペアマッチ 1出力
+	
+	/**
+	 * IMF フラグ割込 要求許可
+	T_TISRA.BIT.IMIE = 0x01; //割込 要求許可
+	T_TISRB.BIT.IMIE = 0x01; //割込 要求許可
+	 */
+	
+	/**
+	 * 周期設定
+	 */
+	T_GRA0_H.BYTE = 0x27; // 10000
+	T_GRA0_L.BYTE = 0x10; 
+	//T_GRA1_H.BYTE = 0x00; //   250
+	//T_GRA1_H.BYTE = 0xFA; 
+	
+	/**
+	 * TIMER START
+	 */
+	T_TSTR.BYTE = 0x01; // 16TCNT0 Start
+	
+	while(1)
+	{
+		puts("T_16TCNT0:");	
+			putxval(T_16TCNT0_H.BYTE,2);
+			putxval(T_16TCNT0_L.BYTE,2);
+		puts("\n");	
+		if(T_TISRA.BIT.IMF == 0x01)
+		{
+			
+			T_16TCR0_COUNTER++;
+			puts("T_16TCR0_COUNTER:");	
+				putxval(T_16TCR0_COUNTER,3);
+			puts("\n");	
+			
+			if(T_16TCR0_COUNTER >= 25)
+			{
+				
+				P4DR.BIT.B6 	=~ P4DR.BIT.B6;
+				P4DR.BIT.B7 	=~ P4DR.BIT.B7;
+				T_16TCR0_COUNTER = 0;
+				secCount++;
+				puts("----------------secCount:");	
+					putxval(secCount,3);
+				puts("\n");	
+				/*
+				if(secCount > 10)
+				{
+					break;
+				}
+				*/
+			}
+			
+		}
+	}
+	
+  return 0;	
 }
